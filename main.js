aa94e2bc3f632e318bc5b2ea51d0f1a8ca603bc2
Lola Gasanova <63504067+GasanovaLola@users.noreply.github.com>
2020-11-18 22:29:00 +0200

    add some text

---
+++ main.js
@@ -0,0 +1,16 @@
+/*Cоздать глобальную ф-ю greeting 
+которая каждые 5000ms смотри в тег input с id='name' 
+(получить при помощи document.querySelector('input[id="name"]')) 
+берет значение по ключу value и выводит сообщение преведствия из глобальной переменной greetingMessage.
+alrt(greetingMessage + input.value)*/ 
+
+var greetingMessage = "Hellow";
+var input = {
+    value: document.querySelector('input[id="name"]')
+}
+
+function greeting(greetingMessage) {
+    alert(greetingMessage + " " + this.value.value);
+}
+
+setInterval(greeting.bind(input, greetingMessage), 5000);
