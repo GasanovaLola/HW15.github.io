88095e7a3fc4224703f61556be184f3a68df8da2
Lola Gasanova <63504067+GasanovaLola@users.noreply.github.com>
2020-11-18 23:00:44 +0200

    dshgfu

--- main.js
+++ main.js
@@ -4,7 +4,8 @@
 которая каждые 5000ms смотри в тег input с id='name'  
 (получить при помощи document.querySelector('input[id="name"]')) 
 берет значение по ключу value и выводит сообщение преведствия из глобальной переменной greetingMessage.
-alrt(greetingMessage + input.value)*/ 
+alrt(greetingMessage + input.value)*/  
+
 
 var greetingMessage = "Hellow";
 var input = {
